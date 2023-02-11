b08c8eb89dd735fd4f18792ca6abcc6663da1033
999anmol <anmolmaan808@gmail.com>
2023-02-11 13:07:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const BookForm = () => {
+const [title, setTitle] = useState("");
+const [author, setAuthor] = useState("");
+const [isbn, setIsbn] = useState("");
+const [books, setBooks] = useState([]);
 
-export default App;
+const handleSubmit = e => {
+e.preventDefault();
+setBooks([...books, { title, author, isbn }]);
+setTitle("");
+setAuthor("");
+setIsbn("");
+};
+
+const handleDelete = index => {
+setBooks(books.filter((_, i) => i !== index));
+};
+
+return (
+<>
+<form onSubmit={handleSubmit}>
+<div>
+<label htmlFor="title">Title:</label>
+<input
+type="text"
+id="title"
+value={title}
+onChange={e => setTitle(e.target.value)}
+/>
+</div>
+<div>
+<label htmlFor="author">Author:</label>
+<input
+type="text"
+id="author"
+value={author}
+onChange={e => setAuthor(e.target.value)}
+/>
+</div>
+<div>
+<label htmlFor="isbn">ISBN:</label>
+<input
+type="text"
+id="isbn"
+value={isbn}
+onChange={e => setIsbn(e.target.value)}
+/>
+</div>
+<button type="submit">Add Book</button>
+</form>
+<hr />
+<h3>Added Books</h3>
+<ul>
+{books.map((book, index) => (
+<li key={index}>
+{book.title} by {book.author} ({book.isbn}){" "}
+<button onClick={() => handleDelete(index)}>Delete</button>
+</li>
+))}
+</ul>
+</>
+);
+};
+
+export default BookForm;
+
+
+
